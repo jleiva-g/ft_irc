892d5a073860eacec41dedc75f140c9dc9662183
Jesus Leiva Guerrero <jleiva-g@c2r3s4.42malaga.com>
2026-08-28 17:51:59 +0200

    refactor delegate server checks to server

--- src/Command.cpp
+++ src/Command.cpp
@@ -2,10 +2,14 @@
 #include "Server.hpp"
 #include "Client.hpp"
 
+#include <cctype>
 #include <sstream>
 
-using std::stringstream;
+using std::toupper;
+using std::isalpha;
+using std::isalnum;
 using std::getline;
+using std::stringstream;
 
 static void	parseCommand(const string& raw, string& name, vector<string>& args) {
 	string	line = raw;
@@ -25,6 +29,8 @@ static void	parseCommand(const string& raw, string& name, vector<string>& args) 
 	}
 
 	name = line.substr(pos, end - pos);
+	for (size_t i = 0; i < name.size(); ++i)
+		name[i] = std::toupper(static_cast<unsigned char>(name[i]));
 	pos = end;
 
 	while (pos < line.size()) {
@@ -46,48 +52,19 @@ static void	parseCommand(const string& raw, string& name, vector<string>& args) 
 	}
 }
 
-static void	tryRegister(Client& client) {
+static void	tryRegister(Client& client, Server& server) {
 	if (client.isPassAccepted()
 		&& !client.getNickname().empty()
 		&& !client.getUsername().empty()
-		&& !client.isRegistered()) {
-		client.setRegistered(true);
-		// 001 RPL_WELCOME
-	}
+		&& !client.isRegistered())
+		server.setRegistered(client);
 }
 
+// PASS <password>
 static void	handlePass(Client& client, const Server& server, const vector<string>& args) {
-	if (args.size() != 1) {
-		// 461 ERR_NEEDMOREPARAMS
+	if (args.size() > 1)
 		return;
-	}
-	if (args[0] != server.getPassword()) {
-		// 464 ERR_PASSWDMISMATCH
-		return;
-	}
-	client.setPassAccepted(true);
-	tryRegister(client);
-}
-
-static void	handleNick(Client& client, const Server& server, const vector<string>& args) {
-	if (args.size() != 1 || args[0].empty()) {
-		// 431 ERR_NONICKNAMEGIVEN
-		return;
-	}
-	if (server.isValidNickname(args[0])) {
-		// 432 ERR_ERRONEUSNICKNAME
-		return;
-	}
-	if (server.isNicknameTaken(args[0])) {
-		// 433 ERR_NICKNAMEINUSE
-		return;
-	}
-	client.setNickname(args[0]);
-	tryRegister(client);
-}
-
-static void	handleUser(Client& client, const vector<string>& args) {
-	if (args.size() != 4) {
+	if (args.empty()) {
 		// 461 ERR_NEEDMOREPARAMS
 		return;
 	}
@@ -95,17 +72,88 @@ static void	handleUser(Client& client, const vector<string>& args) {
 		// 462 ERR_ALREADYREGISTRED
 		return;
 	}
-	client.setUsername(args[0]);
-	tryRegister(client);
+	// pass match?	464 ERR_PASSWDMISMATCH
+	server.setPassAccepted(client, args[0]);
+	tryRegister(client, server);
 }
 
+static bool	isSpecial(const char& c) {
+	return (c == '[' || c == ']' || c == '\\' || c == '`'
+		|| c == '^' || c == '_' || c == '{' || c == '|');
+}
+
+static bool	isValidNickname(const string& nickname) {
+	if (nickname.empty() || nickname.size() > 9)
+		return false;
+
+	unsigned char	c = static_cast<unsigned char>(nickname[0]);
+	if (!isalpha(c) && !isSpecial(c))
+		return false;
+
+	for (size_t i = 1; i < nickname.size(); i++) {
+		c = static_cast<unsigned char>(nickname[i]);
+		if (!isalnum(c) && !isSpecial(c) && c != '-')
+			return false;
+	}
+	return true;
+}
+
+// NICK <nickname>
+static void	handleNick(Client& client, Server& server, const vector<string>& args) {
+	if (args.empty() || args[0].empty()) {
+		// 431 ERR_NONICKNAMEGIVEN
+		return;
+	}
+	if (args.size() > 1)
+		return;
+	if (!isValidNickname(args[0])) {
+		// 432 ERR_ERRONEUSNICKNAME
+		return;
+	}
+	// is nickname taken? 433 ERR_NICKNAMEINUSE
+	// server.setNickname(client, args[0]);
+	tryRegister(client, server);
+}
+
+// USER <username> <mode> <unused> <realname>
+static void	handleUser(Client& client, const Server& server, const vector<string>& args) {
+	if (args.size() < 4) {
+		// 461 ERR_NEEDMOREPARAMS
+		return;
+	}
+	if (args.size() > 4)
+		return;
+	if (client.isRegistered()) {
+		// 462 ERR_ALREADYREGISTRED
+		return;
+	}
+	client.setUsername(args[0]);
+	tryRegister(client, server);
+}
+
+static bool isValidChannelName(const string& name)
+{
+	if (name.empty() || name.size() > 50
+		|| (name[0] != '&' && name[0] != '#' && name[0] != '+' && name[0] != '!')
+		|| name.find('\7') != string::npos || name.find(':') != string::npos)
+		return false;
+	return true;
+}
+
+// JOIN <channel>{,<channel>} [<key>{,<key>}]
 static void	handleJoin(Client& client, Server& server, const vector<string>& args) {
 	if (!client.isRegistered()) {
 		// 451 ERR_NOTREGISTERED
 		return;
 	}
-	if (args.size() < 1) {
+	if (args.empty()) {
 		// 461 ERR_NEEDMOREPARAMS
+		return;
+	}
+	if (args.size() > 2)
+		return;
+	if (args[0] == "0") {
+		// server.leaveAllChannels(client);
 		return;
 	}
 
@@ -120,12 +168,20 @@ static void	handleJoin(Client& client, Server& server, const vector<string>& arg
 		key.clear();
 		if (args.size() > 1)
 			getline(keys, key, ',');
-		if (name.empty())
+		if (!isValidChannelName(name)) {
+			// 476 ERR_BADCHANMASK
 			continue;
-		// join channel
+		}
+		// channel limit?	405 ERR_TOOMANYCHANNELS
+		// +i?				473 ERR_INVITEONLYCHAN
+		// wrong key?		475 ERR_BADCHANNELKEY
+		// user limit?		471 ERR_CHANNELISFULL
+		// channel exists?	create if needed
+		// server.joinChannel(client, name, key);
 	}
 }
 
+// PRIVMSG <target>{,<target>} <text>
 static void	handlePrivmsg(Client& client, const Server& server, const vector<string>& args) {
 	if (!client.isRegistered()) {
 		// 451 ERR_NOTREGISTERED
@@ -135,6 +191,8 @@ static void	handlePrivmsg(Client& client, const Server& server, const vector<str
 		// 411 ERR_NORECIPIENT
 		return;
 	}
+	if (args.size() > 2)
+		return;
 	if (args.size() < 2 || args[1].empty()) {
 		// 412 ERR_NOTEXTTOSEND
 		return;
@@ -147,19 +205,13 @@ static void	handlePrivmsg(Client& client, const Server& server, const vector<str
 		if (target.empty())
 			continue;
 		hasTarget = true;
-		if (target[0] == '#') {
-			if (!server.channelExists(target)) {
-				// 403 ERR_NOSUCHCHANNEL
-				continue;
-			}
-			// sendToChannel()
-		} else {
-			if (!server.clientExists(target)) {
-				// 401 ERR_NOSUCHNICK
-				continue;
-			}
-			// sendToClient()
-		}
+		// if (target[0] == '#' || target[0] == '&'
+		// 	|| target[0] == '+' || target[0] == '!')
+		// 	// channel exists? 403 ERR_NOSUCHCHANNEL
+		// 	// server.sendMsgToChannel(client, target, args[1])
+		// else
+		// 	// client exists? 401 ERR_NOSUCHNICK
+		// 	// server.sendMesgToClient(client, target, args[1])
 	}
 	if (!hasTarget) {
 		// 411 ERR_NORECIPIENT
@@ -167,6 +219,7 @@ static void	handlePrivmsg(Client& client, const Server& server, const vector<str
 	}
 }
 
+// KICK <channel> <user>{,<user>} [<comment>]
 static void	handleKick(Client& client, Server& server, const vector<string>& args) {
 	if (!client.isRegistered()) {
 		// 451 ERR_NOTREGISTERED
@@ -176,40 +229,31 @@ static void	handleKick(Client& client, Server& server, const vector<string>& arg
 		// 461 ERR_NEEDMOREPARAMS
 		return;
 	}
-	if (!server.channelExists(args[0])) {
-		// 403 ERR_NOSUCHCHANNEL
+	if (args.size() > 3)
 		return;
-	}
-	if (!server.isClientInChannel(client, args[0])) {
-		// 442 ERR_NOTONCHANNEL
-		return;
-	}
-	if (!server.isChannelOp(client, args[0])) {
-		// 482 ERR_CHANOPRIVSNEEDED
-		return;
-	}
 
 	stringstream	users(args[1]);
 	string			nickname;
+	vector<string>	nicknames;
 	string			comment;
 	if (args.size() >= 3)
 		comment = args[2];
 	while (getline(users, nickname, ',')) {
-		if (nickname.empty())
-			continue;
-		if (!server.clientExists(nickname)) {
-			// 401 ERR_NOSUCHNICK
-			continue;
-		}
-		if (!server.isClientInChannel(nickname, args[0])) {
-			// 441 ERR_USERNOTINCHANNEL
-			continue;
-		}
-		// server.kickClient(client, nickname, args[0], comment);
+		if (!nickname.empty())
+			nicknames.push_back(nickname);
 	}
+	if (nicknames.empty())
+		return;
+	// channel exists?			403 ERR_NOSUCHCHANNEL
+	// is kicker in channel?	442 ERR_NOTONCHANNEL
+	// is kicker op?			482 ERR_CHANOPRIVSNEEDED
+	// target exists?			401 ERR_NOSUCHNICK
+	// is target in channel?	441 ERR_USERNOTINCHANNEL
+	// server.kickClient(client, args[0], nicknames, comment);
 }
 
-static void	handleinvite(Client& client, Server& server, const vector<string>& args) {
+// INVITE <nickname> <channel>
+static void	handleInvite(Client& client, Server& server, const vector<string>& args) {
 	if (!client.isRegistered()) {
 		// 451 ERR_NOTREGISTERED
 		return;
@@ -218,27 +262,45 @@ static void	handleinvite(Client& client, Server& server, const vector<string>& a
 		// 461 ERR_NEEDMOREPARAMS
 		return;
 	}
-	if (!server.channelExists(args[1])) {
-		// 403 ERR_NOSUCHCHANNEL
+	if (args.size() > 2)
 		return;
-	}
-	if (!server.clientExists(args[0])) {
-		// 401 ERR_NOSUCHNICK
-		return;
-	}
-	if (!server.isClientInChannel(client, args[1])) {
-		// 442 ERR_NOTONCHANNEL
-		return;
-	}
-	if (!server.isChannelOp(client, args[1])) {
-		// 482 ERR_CHANOPRIVSNEEDED
-		return;
-	}
-	if (server.isClientInChannel(args[0], args[1])) {
-		// 443 ERR_USERONCHANNEL
-		return;
-	}
+
+	// channel exists?		403 ERR_NOSUCHCHANNEL
+	// target exists?		401 ERR_NOSUCHNICK
+	// inviter in channel?	442 ERR_NOTONCHANNEL
+	// inviter op if +i?	482 ERR_CHANOPRIVSNEEDED
+	// target in channel?	443 ERR_USERONCHANNEL
 	// server.inviteClient(client, args[0], args[1])
+}
+
+// TOPIC <channel> [<topic>]
+static void	handleTopic(Client& client, Server& server, const vector<string>& args) {
+	if (!client.isRegistered()) {
+		// 451 ERR_NOTREGISTERED
+		return;
+	}
+	if (args.empty()) {
+		// 461 ERR_NEEDMOREPARAMS
+		return;
+	}
+	if (args.size() > 2)
+		return;
+
+	if (args.size() == 1) {
+		// channel exists?		403 ERR_NOSUCHCHANNEL
+		// client in channel?	442 ERR_NOTONCHANNEL
+		// server.sendTopic(client, channel);
+		return;
+	}
+	// channel exists?		403 ERR_NOSUCHCHANNEL
+	// client in channel?	442 ERR_NOTONCHANNEL
+	// client op if +t?		482 ERR_CHANOPRIVSNEEDED
+	// broadcast topic
+	// server.setTopic(client, args[0], args[1]);
+}
+
+// MODE <channel> <mode> [<mode parameters>...]
+static void	handleMode(Client& client, Server& server, const vector<string>& args) {
 }
 
 void	Command::handleCommand(Client& client, Server& server, const string& raw) {
@@ -251,7 +313,7 @@ void	Command::handleCommand(Client& client, Server& server, const string& raw) {
 	else if (name == "NICK")
 		handleNick(client, server, args);
 	else if (name == "USER")
-		handleUser(client, args);
+		handleUser(client, server, args);
 	else if (name == "JOIN")
 		handleJoin(client, server, args);
 	else if (name == "PRIVMSG")

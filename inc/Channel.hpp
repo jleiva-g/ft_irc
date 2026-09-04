1b02e22b9b9e99bb0d4d648a0b81b85f39dd2869
emiliogburgos <emiliojaviergarciaburgos@gmail.com>
2026-09-04 11:28:36 +0200

    changes in the mode managemente, and swiched functions to cpp for visual clarity

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -1,48 +1,59 @@
-#pragma once
+	#pragma once
 
-#include <string>
-#include <set>
+	#include <string>
+	#include <set>
 
-using std::string;
-using std::set;
+	using std::string;
+	using std::set;
 
-class Client;
+	class Client;
 
-class	Channel {
-	private:
-		string			name;
-		string			topic;
-		string			key;
-		set<Client*>	members;
-		set<Client*>	operators;
-		bool			inviteOnly;
-		bool			topicOpOnly;
-		bool			hasKey;
-		long			userLimit;
-	public:
-		Channel();
-		Channel(const string& name);
-		void				addMember(Client* client);
-		void				removeMember(Client* client);
-		void				addOperator(Client* client);
-		void				removeOperator(Client* client);
-		bool				isMember(Client* client) const;
-		bool				isOperator(Client* client) const;
-		const string&		getName() const { return name; }
-		const string&		getTopic() const { return topic; }
-		const set<Client*>&	getMembers() const { return members; }
-		const set<Client*>&	getOperators() const { return operators; }
-		bool				isInviteOnly() const { return inviteOnly; }
-		bool				isTopicOpOnly() const { return topicOpOnly; }
-		bool				itHasKey() const { return hasKey; }
-		bool				itHasUserLimit() const { return userLimit != -1; }
-		const string&		getKey() const { return key; }
-		size_t				getUserLimit() const { return userLimit; }
-		void				setName(const string& value) { name = value; }
-		void				setTopic(const string& value) { topic = value; }
-		void				setKey(const string& value) { key = value; }
-		void				setInviteOnly(bool value) { inviteOnly = value; }
-		void				setTopicOpOnly(bool value) { topicOpOnly = value; }
-		void				setHasKey(bool value) { hasKey = value; }
-		void				setUserLimit(size_t value) { userLimit = value; }
-};
+	class Channel {
+		private:
+			enum Mode {
+				MODE_INVITE_ONLY = 1 << 0,
+				MODE_TOPIC_OP_ONLY = 1 << 1,
+				MODE_KEY = 1 << 2,
+				MODE_USER_LIMIT = 1 << 3
+			};
+			string			name;
+			string			topic;
+			string			key;
+			set<Client*>	members;
+			set<Client*>	operators;
+			unsigned int	modes;
+			long			userLimit;
+
+		public:
+			Channel();
+			Channel(const string& name);
+
+			void				addMember(Client* client);
+			void				removeMember(Client* client);
+			void				addOperator(Client* client);
+			void				removeOperator(Client* client);
+			void				changeMode(char mode, bool enable);
+
+			const string&		getName() const;
+			const string&		getTopic() const;
+			const set<Client*>&	getMembers() const;
+			const set<Client*>&	getOperators() const;
+			const string&		getKey() const;
+			size_t				getUserLimit() const;
+
+			bool				isMember(Client* client) const;
+			bool				isOperator(Client* client) const;
+
+			bool				isInviteOnly() const;
+			bool				isTopicOpOnly() const;
+			bool				itHasKey() const;
+			bool				itHasUserLimit() const;
+
+			void				setName(const string& value);
+			void				setTopic(const string& value);
+			void				setKey(const string& value);
+			void				setInviteOnly(bool value);
+			void				setTopicOpOnly(bool value);
+			void				setHasKey(bool value);
+			void				setUserLimit(size_t value);
+	};
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -6,10 +6,7 @@ Channel::Channel()
       key(),
       members(),
       operators(),
-      inviteOnly(false),
-      topicOpOnly(false),
-      hasKey(false),
-      hasUserLimit(false),
+      modes(0),
       userLimit(-1)
 {}
 
@@ -19,25 +16,179 @@ Channel::Channel(const string& name)
       key(),
       members(),
       operators(),
-      inviteOnly(false),
-      topicOpOnly(false),
-      hasKey(false),
-      hasUserLimit(false),
+      modes(0),
       userLimit(-1)
 {}
 
+/**
+ * @brief Changes a channel mode.
+ * @details Enables or disables the bit associated with the supplied mode
+ *  without changing any other active mode.
+ *
+ * @param[in] mode Mode character: `i`, `t`, `k` or `l`.
+ * @param[in] enable True to enable the mode, false to disable it.
+ */
+void Channel::changeMode(char mode, bool enable)
+{
+    unsigned int modeBit;
+
+    if (mode == 'i')
+        modeBit = MODE_INVITE_ONLY;
+    else if (mode == 't')
+        modeBit = MODE_TOPIC_OP_ONLY;
+    else if (mode == 'k')
+        modeBit = MODE_KEY;
+    else if (mode == 'l')
+        modeBit = MODE_USER_LIMIT;
+    else
+        return;
+
+    if (enable)
+        modes |= modeBit;
+    else
+        modes &= ~modeBit;
+}
+
+const string& Channel::getName() const
+{
+  return name;
+}
+
+const string& Channel::getTopic() const
+{
+  return topic;
+}
+
+const set<Client*>& Channel::getMembers() const
+{
+  return members;
+}
+
+const set<Client*>& Channel::getOperators() const
+{
+  return operators;
+}
+
+const string& Channel::getKey() const
+{
+  return key;
+}
+
+size_t Channel::getUserLimit() const
+{
+  return userLimit;
+}
+
+bool Channel::isInviteOnly() const
+{
+  return (modes & MODE_INVITE_ONLY) != 0;
+}
+
+bool Channel::isTopicOpOnly() const
+{
+  return (modes & MODE_TOPIC_OP_ONLY) != 0;
+}
+
+bool Channel::itHasKey() const
+{
+  return (modes & MODE_KEY) != 0;
+}
+
+bool Channel::itHasUserLimit() const
+{
+  return (modes & MODE_USER_LIMIT) != 0;
+}
+
+/**
+ * @brief Sets the channel name.
+ * @param[in] value New channel name.
+ */
+void Channel::setName(const string& value)
+{
+  name = value;
+}
+
+/**
+ * @brief Sets the channel topic.
+ * @param[in] value New channel topic.
+ */
+void Channel::setTopic(const string& value)
+{
+  topic = value;
+}
+
+/**
+ * @brief Sets the channel key.
+ * @param[in] value New channel key.
+ */
+void Channel::setKey(const string& value)
+{
+  key = value;
+}
+
+/**
+ * @brief Enables or disables invite-only mode.
+ * @param[in] value True to enable invite-only mode.
+ */
+void Channel::setInviteOnly(bool value)
+{
+  changeMode('i', value);
+}
+
+/**
+ * @brief Enables or disables operator-only topic mode.
+ * @param[in] value True to enable operator-only topic mode.
+ */
+void Channel::setTopicOpOnly(bool value)
+{
+  changeMode('t', value);
+}
+
+/**
+ * @brief Enables or disables key protection.
+ * @param[in] value True to enable key protection.
+ */
+void Channel::setHasKey(bool value)
+{
+  changeMode('k', value);
+}
+
+/**
+ * @brief Sets the channel user limit.
+ * @param[in] value Maximum number of users allowed in the channel.
+ */
+void Channel::setUserLimit(size_t value)
+{
+  userLimit = value;
+}
+
+/**
+ * @brief Adds a client to the channel.
+ * @details Null client pointers are ignored.
+ * @param[in] client Client to add to the channel.
+ */
 void Channel::addMember(Client* client)
 {
   if (client)
     members.insert(client);
 }
 
+/**
+ * @brief Removes a client from the channel.
+ * @details A removed client is also removed from the operator set.
+ * @param[in] client Client to remove from the channel.
+ */
 void Channel::removeMember(Client* client)
 {
     members.erase(client);
     operators.erase(client);
 }
 
+/**
+ * @brief Checks whether a client belongs to the channel.
+ * @param[in] client Client to look up.
+ * @return True when the client is a channel member.
+ */
 bool Channel::isMember(Client* client) const
 {
   if (members.find(client) != members.end())
@@ -46,6 +197,11 @@ bool Channel::isMember(Client* client) const
     return false;
 }
 
+/**
+ * @brief Checks whether a client has channel operator status.
+ * @param[in] client Client to look up.
+ * @return True when the client is a channel operator.
+ */
 bool Channel::isOperator(Client* client) const
 {
   if (operators.find(client) != operators.end())
@@ -54,12 +210,21 @@ bool Channel::isOperator(Client* client) const
     return false;
 }
 
+/**
+ * @brief Grants channel operator status to a member.
+ * @details Clients that are not channel members are ignored.
+ * @param[in] client Member to promote to channel operator.
+ */
 void Channel::addOperator(Client* client)
 {
     if (isMember(client))
         operators.insert(client);
 }
 
+/**
+ * @brief Revokes channel operator status from a client.
+ * @param[in] client Client whose operator status is removed.
+ */
 void Channel::removeOperator(Client* client)
 {
 	operators.erase(client);
